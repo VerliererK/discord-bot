c5613c44764bae83b6783f8f1ae435c5b5f43fe9
Cheng-Kai Lin <kai.lin@tg3ds.com>
2023-01-16 14:52:50 +0800

    Init project

---
+++ server.js
@@ -0,0 +1,25 @@
+const { Client, Events, Partials, GatewayIntentBits } = require('discord.js');
+
+
+const client = new Client({
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content == 'Hello')
+    message.channel.send('World!');
+});
+
+client.login(process.env.BOT_TOKEN);
